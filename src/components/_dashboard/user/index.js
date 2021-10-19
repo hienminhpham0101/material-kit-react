8971351ca2627aeceaba97535a3b834da07c7eba
Hien Pham <hienminhpham0101@gmail.com>
2021-10-19 16:09:48 +0700

    remove redunt

--- src/components/_dashboard/user/index.js
+++
@@ -1,3 +0,0 @@
-export { default as UserListHead } from './UserListHead';
-export { default as PostListToolbar } from './PostListToolbar';
-export { default as UserMoreMenu } from './UserMoreMenu';
--- src/components/animate/variants/bounce/BounceOut.js
+++ src/components/animate/variants/bounce/BounceOut.js
@@ -6,35 +6,3 @@ export const varBounceOut = {
     opacity: [1, 1, 0]
   }
 };
-
-export const varBounceOutUp = {
-  animate: {
-    y: [-12, 24, -720],
-    scaleY: [0.985, 0.9, 3],
-    opacity: [1, 1, 0]
-  }
-};
-
-export const varBounceOutDown = {
-  animate: {
-    y: [12, -24, 720],
-    scaleY: [0.985, 0.9, 3],
-    opacity: [1, 1, 0]
-  }
-};
-
-export const varBounceOutLeft = {
-  animate: {
-    x: [0, 24, -720],
-    scaleX: [1, 0.9, 2],
-    opacity: [1, 1, 0]
-  }
-};
-
-export const varBounceOutRight = {
-  animate: {
-    x: [0, -24, 720],
-    scaleX: [1, 0.9, 2],
-    opacity: [1, 1, 0]
-  }
-};
